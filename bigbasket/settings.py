766a2d300b840da9fcd6758758af57a9239032e1
jawaharrohan25 <86509499+jawaharrohan25@users.noreply.github.com>
2021-07-05 16:38:44 +0300

    test

--- bigbasket/settings.py
+++ bigbasket/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-dg+$h=p)58i2gw-#vt9%$4co16vv%s9p7h7$0gpw-o+1($rieg
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["jawaharrohan.pythonanywhere.com"]
 
 
 # Application definition
